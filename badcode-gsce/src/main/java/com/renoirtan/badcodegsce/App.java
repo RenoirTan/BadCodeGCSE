314b18dcb2ba05785f0727d708fae2348668c1b6
RenoirTan <renoirtan2005@gmail.com>
2021-07-03 21:00:10 +0800

    main: change entry point class' main function to redirect to one of the 3 tasks

--- badcode-gsce/src/main/java/com/renoirtan/badcodegsce/App.java
+++ badcode-gsce/src/main/java/com/renoirtan/badcodegsce/App.java
@@ -1,11 +1,28 @@
 package com.renoirtan.badcodegsce;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String task;
+        try {
+            task = args[0];
+        } catch (Exception e) {
+            System.out.println("Please provide a task you want to run.");
+            return;
+        }
+        String[] taskArgs = Arrays.copyOfRange(args, 1, args.length + 1);
+        switch (task) {
+            case "musicquiz":
+                com.renoirtan.badcodegsce.musicquiz.App.main(taskArgs);
+                break;
+            default:
+                System.out.println("Unknown task: " + task);
+                break;
+        }
     }
 }
